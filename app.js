3233979877b7c4f05c96c27eccb21c817e1dd940
Try <khovct@gmail.com>
2019-08-07 14:56:17 -0700

    connected to mongoDB

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 const express = require('express');
-
 const app = express();
-
+const mongoose = require('mongoose');
+require('dotenv/config');
 // Have ability to create routes in simple way
 
+// Middlewares
+
+
+// app.use('/posts', (req, res) => {
+//     // every time posts is called / visited, this logic runs
+//     console.log('This is a middleware running');
+//     res.send("We're on posts");
+// })
+
 // Routes
-app.get('/', (req, res) => {
-    res.send("We are on home");
+
+// get, post, delete, patch are methods
+// first method is route
+app.get('/', (req, res) => { 
+    res.send("We are on home"); 
+});
+
+app.get('/posts', (req, res) => {
+    res.send("We're on posts");
+});
+
+
+// Connect to DB
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
+    console.log('connected to DB!');
+    
 })
-
-
-
 
 // Listening to server
 app.listen(3000);
